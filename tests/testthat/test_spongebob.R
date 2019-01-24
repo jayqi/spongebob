8498eec7dbc15e30215a1a2d4a6aaa3085271052
Jay Qi <jayqi@users.noreply.github.com>
2019-01-23 22:07:06 -0600

    Add tests for error for non-character input

--- tests/testthat/test_spongebob.R
+++ tests/testthat/test_spongebob.R
@@ -3,10 +3,23 @@ context("tospongebob")
 test_that("tospongebob runs end-to-end successfully", {
     expect_true({
         tospongebob("tospongebob runs end-to-end successfully")
+        TRUE
+    })
+    expect_true({
         tospongebob(c(
             "i really hope that this test passes",
             "or else somebody is going to have to fix it"
         ))
         TRUE
     })
+})
+
+test_that("tospongebob errors for non-character inputs", {
+    msg <- "not a character vector"
+    expect_error(tospongebob(123), regexp = msg)
+    expect_error(tospongebob(c(TRUE, FALSE)), regexp = msg)
+    expect_error(
+        tospongebob(list("char in list", "char in list"))
+        , regexp = msg
+    )
 })
